3272d4cfeb53a17e5d9dbcbef841d94888295861
李明阳 <limingyang@dhcp-10-105-153-184.wireless.northwestern.private>
2017-11-17 16:23:26 -0600

    test

--- service/main.go
+++ service/main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"strconv"
 	"reflect"
+	"strings"
 )
 
 type Location struct {
@@ -127,7 +128,9 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 		p := item.(Post) // p = (Post) item
 		fmt.Printf("Post by %s: %s at lat %v and lon %v\n", p.User, p.Message, p.Location.Lat, p.Location.Lon)
 		// TODO(student homework): Perform filtering based on keywords such as web spam etc.
-		ps = append(ps, p)
+		if !containsFilteredWords(&p.Message) {
+			ps = append(ps, p)
+		}
 
 	}
 	js, err := json.Marshal(ps)
@@ -140,6 +143,19 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write(js)
 
+}
+
+func containsFilteredWords(s *string) bool {
+	filteredWords := []string{
+		"fuck",
+		"100",
+	}
+	for _, word := range filteredWords {
+		if strings.Contains(*s, word) {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
