bda88c0a60f4c07fe9314e0a563a7d07a051c36e
李明阳 <limingyang@dhcp-10-105-153-184.wireless.northwestern.private>
2017-11-19 17:06:34 -0600

    test

--- service/main.go
+++ service/main.go
@@ -34,7 +34,7 @@ const (
 	PROJECT_ID = "around-185906"
 	BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.196.24.165:9200"
+	ES_URL = "http://35.196.193.48:9200"
 )
 
 
